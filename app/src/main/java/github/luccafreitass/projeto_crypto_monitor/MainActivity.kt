a317de587cddd921e751974441057569c79140e1
luccaFreitass <luccadefreitasfaria@hotmail.com>
2024-10-04 21:48:28 -0300

    Configurando a MainActivity.kt para executar com o layout pronto

--- app/src/main/java/github/luccafreitass/projeto_crypto_monitor/MainActivity.kt
+++ app/src/main/java/github/luccafreitass/projeto_crypto_monitor/MainActivity.kt
@@ -1,22 +1,22 @@
 package github.luccafreitass.projeto_crypto_monitor
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import github.luccafreitass.projeto_crypto_monitor.ui.theme.ProjetoCryptoMonitorTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setTitle(getText(R.string.app_title))
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
     }
 }
